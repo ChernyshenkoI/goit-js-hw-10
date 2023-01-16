a39c3a15577138461c6a8d0d090e0f3fb93ab329
Iryna C <106169218+ChernyshenkoI@users.noreply.github.com>
2023-01-17 00:04:43 +0200

    final variant

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './css/styles.css';
-import { fetchCountries } from './js/fetchCountries.js'
+import { fetchCountries } from './js/fetchCountries.js';
 import { Notify } from 'notiflix';
-
 
 const debounce = require('lodash.debounce');
 const DEBOUNCE_DELAY = 300;
-
 
 const refs = {
     countryInput: document.querySelector('#search-box'),
@@ -14,8 +12,10 @@ const refs = {
 
 refs.countryInput.addEventListener('input', debounce(onInputSearch, DEBOUNCE_DELAY));
 
-function onInputSearch(e) {
-    const countryToSearch = e.target.value.trim();
+function onInputSearch(event) {
+    event.preventDefault();
+
+    const countryToSearch = event.target.value.trim();
 
     if (countryToSearch === "") {
         clearCountries();
@@ -35,7 +35,7 @@ function onInputSearch(e) {
         })
         .catch(() => {
             Notify.failure("Oops, there is no country with that name");
-            e.target.value = "";
+            event.target.value = "";
             clearCountries();
         })
 }
@@ -43,25 +43,25 @@ function onInputSearch(e) {
 function renderCountries(countries) {
     const countryList = countries.map(country =>
         `<li>
-        <img width=30px src="${country.flags.svg}" 
-        alt="${country.name.official}"> 
+        <img width=30px src="${country.flags.svg}" alt="${country.name.official}"> 
         <span>${country.name.official}</span>
-        </li>`).join('');
+        </li>`
+        ).join('');
+
     refs.countries.innerHTML = countryList;
 }
 
-
 function renderDetailCountry(countries) {
-    const country = countries.map(country => { 
+    const country = countries.map(country => {
         return `<li>
-                <img width=30px src="${country.flags.svg}" 
-                alt="${country.name.official}"></img>
+                <img width=30px src="${country.flags.svg}" alt="${country.name.official}"></img>
                 <h2 style="display: inline-block">${country.name.official}</h2>
                 <p>Capital: ${country.capital}</p>
                 <p>Population: ${country.population}</p>
                 <p>Languages: ${Object.values(country.languages)}</p>
                 </li>`
     }).join('');
+
     refs.countries.innerHTML = country;
 }
 
